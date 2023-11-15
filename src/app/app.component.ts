b83ba3ecec004beeb0993f6eee333b3dc30e4581
shohan <shohan.drmc@gmail.com>
2023-11-15 20:55:36 +0800

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
-import { environment } from "../environments/environment";
-import { getMessaging, getToken, onMessage } from "firebase/messaging";
+import {Component, OnInit} from '@angular/core';
+import {environment} from "../environments/environment";
+import {getMessaging, getToken, onMessage} from "firebase/messaging";
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -8,20 +9,24 @@ import { getMessaging, getToken, onMessage } from "firebase/messaging";
 })
 export class AppComponent implements OnInit {
   title = 'af-notification';
-  message:any = null;
-  constructor() {}
+  message: any = null;
+
+  constructor() {
+  }
+
   ngOnInit(): void {
     this.requestPermission();
     this.listen();
   }
+
   requestPermission() {
     const messaging = getMessaging();
     getToken(messaging,
-      { vapidKey: environment.firebase.vapidKey}).then(
+      {vapidKey: environment.firebase.vapidKey}).then(
       (currentToken) => {
         if (currentToken) {
           console.log("Hurraaa!!! we got the token.....");
-          console.log(currentToken);
+          console.log({currentToken});
         } else {
           console.log('No registration token available. Request permission to generate one.');
         }
@@ -29,11 +34,12 @@ export class AppComponent implements OnInit {
       console.log('An error occurred while retrieving token. ', err);
     });
   }
+
   listen() {
     const messaging = getMessaging();
     onMessage(messaging, (payload) => {
       console.log('Message received. ', payload);
-      this.message=payload;
+      this.message = payload;
     });
   }
 }
