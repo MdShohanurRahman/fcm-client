200e343884b888ba3b39343970472901a15ee8c6
shohan <shohan.drmc@gmail.com>
2023-11-16 17:03:05 +0800

    update

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,13 +1,13 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: "AIzaSyBVcIeYTgCOK4CHRcKgv4fUbXvL1t8shLE",
-    authDomain: "push-notify-3ef28.firebaseapp.com",
-    projectId: "push-notify-3ef28",
-    storageBucket: "push-notify-3ef28.appspot.com",
-    messagingSenderId: "799739220120",
-    appId: "1:799739220120:web:f28b4620ca26d447bc40ed",
-    measurementId: "G-PPVCRKEKX6",
-    vapidKey: "BFelLNdjd2C-Q4udIVv89xqWef5d1_ffBQ3up5H9KvAgJmf3e32AzMwjYJ0Ea_abT5_kxb4uDTCXlIK7zfM0VQY"
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: "",
+    vapidKey: ""
   }
 };
